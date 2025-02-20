51822adf5f253cb1d6ca0fd030a35fd0b6f4d94a
Michael M Santos <mmss240610@gmail.com>
2025-02-20 11:00:26 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,7 +1,8 @@
 import { notFound } from "next/navigation";
 
-import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -10,30 +11,36 @@ interface RestaurantMenuPageProps {
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-    return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod);
+    return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
 };
 
-const RestaurantMenuPage = async ({ params, searchParams }: RestaurantMenuPageProps) => {
+const RestaurantMenuPage = async ({
+    params,
+    searchParams,
+}: RestaurantMenuPageProps) => {
     const { slug } = await params;
-
     const { consumptionMethod } = await searchParams;
-
     if (!isConsumptionMethodValid(consumptionMethod)) {
         return notFound();
     }
-
-    const restaurant = await getRestaurantBySlug(slug);
+    const restaurant = await db.restaurant.findUnique({
+        where: { slug },
+        include: {
+            menuCategories: {
+                include: { products: true },
+            },
+        },
+    });
     if (!restaurant) {
         return notFound();
     }
-
     return (
         <div>
             <RestaurantHeader restaurant={restaurant} />
+            <RestaurantCategories restaurant={restaurant} />
         </div>
     );
-}
+};
 
 export default RestaurantMenuPage;
-
 
